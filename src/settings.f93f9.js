9781718b214b5af88735ad9c80f6c51be7461ac4
la vie est belle <louasure@126.com>
2024-09-18 23:39:15 +0800

    update

--- src/settings.f93f9.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/editor.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"b69eb",main:"78d5c"}};
